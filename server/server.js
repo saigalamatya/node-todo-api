8fd40d99cd5c57e8ed740d417a706dbef3298f13
Saigal <saigal.amatya@gmail.com>
2018-01-10 17:06:46 +0545

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const { ObjectId } = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -29,6 +31,43 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+// GET /todos/12321
+app.get('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+
+  if (!ObjectId.isValid) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    console.log(todo);
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({
+      status: "200",
+      todo
+    });
+  }).catch((e) => {
+    res.status(400).send({
+      message: "Invalid ID"
+    });
+  });
+
+});
+
+  // Valid id using isValid
+    // 404 - send back an empty body
+
+  // query by findById
+    // success
+      // if todo - send it back
+      // if no todo -send back 404 with empty body
+      // error
+        // 400 - and send empty body back
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
